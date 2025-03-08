56cc612b7312b08e0a823bc38f47e8ca62b8c97b
Andrew <ajmarshall2k@gmail.com>
2025-03-08 13:18:39 -0700

    Add doors

--- src/main/java/model/elevator/Elevator.java
+++ src/main/java/model/elevator/Elevator.java
@@ -1,7 +1,35 @@
 package model.elevator;
 
 public class Elevator {
+
+    private DoorsState doorsState;
+
+    public enum DoorsState {closed, opened}
+
+    public DoorsState getDoorsState() {
+        return doorsState;
+    }
+
+    public Elevator() {
+        doorsState = DoorsState.opened;
+    }
+
     public boolean isTrue() {
         return true;
     }
+
+
+
+    public boolean openDoors() {
+        doorsState = DoorsState.opened;
+        return true;
+    }
+
+    public boolean closeDoors() {
+        doorsState = DoorsState.closed;
+        return true;
+    }
+
 }
+
+;
--- src/test/java/model/elevator/ElevatorTest.java
+++ src/test/java/model/elevator/ElevatorTest.java
@@ -2,6 +2,7 @@ package model.elevator;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ElevatorTest {
@@ -10,5 +11,18 @@ public class ElevatorTest {
     public void testElevator() {
         Elevator elevator = new Elevator();
         assertTrue(elevator.isTrue());
+    }
+
+    @Test
+    public void testElevatorDoorsStartOpened() {
+        Elevator elevator = new Elevator();
+        assertSame(Elevator.DoorsState.opened, elevator.getDoorsState());
+    }
+
+    @Test
+    public void testElevatorDoorsAreClosed() {
+        Elevator elevator = new Elevator();
+        elevator.closeDoors();
+        assertSame(Elevator.DoorsState.closed, elevator.getDoorsState());
     }
 }
